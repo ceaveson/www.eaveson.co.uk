408c3eb38022738d8df7997615e22d888f964095
Craig <ceaveson@gmail.com>
2023-01-09 21:09:19 +0000

    converted from module to a package

--- www_eaveson_co_uk/app.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, request, abort
-from www_eaveson_co_uk import app
-from models import Post
--- www_eaveson_co_uk/blog.py
+++ www_eaveson_co_uk/blog.py
@@ -1,15 +1,27 @@
 from flask import Blueprint, render_template
-from . import date_with_day_suffix, Post
+from . import date_with_day_suffix
+from www_eaveson_co_uk.database import db
+from datetime import datetime
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint('blog', __name__)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
 
 @bp.route("/")
-def hello_world():
-    return render_template("home.html")
+def index():
+    return render_template("blog/home.html")
 
 @bp.route("/test")
 def test():
     posts = Post.query.all()
     for post in posts:
         post.pub_date_str = date_with_day_suffix(post.pub_date)
-    return render_template("posts.html", posts = posts)
+    return render_template("blog/posts.html", posts = posts)
