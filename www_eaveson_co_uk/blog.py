7f8d1ac9d835ee1bf949fa473d23854450d9733e
Craig <ceaveson@gmail.com>
2023-01-12 20:25:18 +0000

    add delete and edit posts

--- www_eaveson_co_uk/blog.py
+++ www_eaveson_co_uk/blog.py
@@ -11,6 +11,7 @@ bp = Blueprint('blog', __name__)
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text, nullable=False)
+    live = db.column(db.Boolean)
     pub_date = db.Column(db.DateTime, nullable=False,
         default=datetime.utcnow)
 
@@ -39,3 +40,24 @@ def add():
         db.session.commit()
         return redirect(url_for('blog.test'))
     return render_template('blog/add_post.html')
+
+@bp.route("/edit/<int:post_id>", methods = ('GET', 'POST'))
+@login_required
+def edit(post_id):
+    if request.method == 'POST':
+        body = request.form['body']
+        post = Post.query.filter_by(id=post_id).first()
+        post.body = body
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('blog.test'))
+    post = Post.query.filter_by(id=post_id).first()
+    return render_template('blog/edit.html', post=post)
+
+@bp.route("/delete/<int:post_id>")
+@login_required
+def delete(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('blog.test'))
